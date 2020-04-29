707061bc5ac044db183fa86b30da03d04b2927e6
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2020-04-30 00:46:24 +0800

    回溯法 - 二叉树中路径和为某个值的路径

---
+++ algorithm-learning/src/main/java/algorithm/backtracking/ThreeColor.java
@@ -0,0 +1,33 @@
+package algorithm.backtracking;
+
+import java.util.ArrayList;
+
+import algorithm.structure.TreeNode;
+
+public class ThreeColor {
+
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        if (root == null) {
+            return list;
+        }
+        find(list, list1, root, target);
+        return list;
+    }
+
+    public void find(ArrayList<ArrayList<Integer>> list, ArrayList<Integer> list1, TreeNode root, int target) {
+        if (root == null) {
+            return;
+        }
+        list1.add(root.var);
+        target -= root.var;
+        if (target == 0 && root.left == null && root.right == null) {
+            list.add(new ArrayList<Integer>(list1));
+        }
+        find(list, list1, root.left, target);
+        find(list, list1, root.right, target);
+        // 复原才能回溯
+        list1.remove(list1.size() - 1);
+    }
+}
--- algorithm-learning/src/main/java/algorithm/structure/TreeNode.java
+++ algorithm-learning/src/main/java/algorithm/structure/TreeNode.java
@@ -4,13 +4,13 @@ package algorithm.structure;
  * Created by n!Ck Date: 2018/10/6 Time: 17:09 Description:
  */
 public class TreeNode {
-    private Integer var;
-    private TreeNode leftChild;
-    private TreeNode rightChild;
+    public Integer var;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(Integer var) {
         this.var = var;
-        this.leftChild = null;
-        this.rightChild = null;
+        this.left = null;
+        this.right = null;
     }
 }
