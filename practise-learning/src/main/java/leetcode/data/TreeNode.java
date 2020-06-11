41b237d1854526b28dbcbfef8d4a74a153425f8e
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2020-06-11 20:50:48 +0800

    Leetcode 572. 另一棵树的子树

--- practise-learning/src/main/java/leetcode/data/TreeNode.java
+++ practise-learning/src/main/java/leetcode/data/TreeNode.java
@@ -11,7 +11,17 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+
+    }
+
     public TreeNode(int x) {
         this.val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = left;
     }
 }
