e095010283c19458422c0f0823350039c40c49f2
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2020-07-06 17:55:36 +0800

    Leetcode 26. 删除排序数组中的重复项

---
+++ practise-learning/src/main/java/leetcode/easy/array/_26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,78 @@
+package leetcode.easy.array;
+
+/**
+ * @author zhanglei
+ * @version 1.0
+ * @description 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定数组 nums = [1,1,2],
+ * <p>
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ * <p>
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ * <p>
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *  
+ * <p>
+ * 说明:
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * <p>
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * <p>
+ * 你可以想象内部操作如下:
+ * <p>
+ * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ * int len = removeDuplicates(nums);
+ * <p>
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2020/7/6 15:55
+ **/
+public class _26_RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int n = removeDuplicates(nums);
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    /**
+     * 双指针原地拷贝
+     *
+     * @param nums 待处理数组
+     * @return 移除相同元素后的新数组长度
+     */
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int j = 0;
+        // 从第二个开始遍历数组
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j-1;
+    }
+}
