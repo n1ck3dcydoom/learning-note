b76a6d1afbb8c6f2efa44a87dc42aa63f0b91858
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2020-08-31 20:14:56 +0800

    Leetcode 42. 接雨水 动态规划解法

--- practise-learning/src/main/java/leetcode/easy/array/_42_TrappingRainWater.java
+++ practise-learning/src/main/java/leetcode/easy/array/_42_TrappingRainWater.java
@@ -18,7 +18,7 @@ package leetcode.easy.array;
 public class _42_TrappingRainWater {
     public static void main(String[] args) {
         int[] nums = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        System.out.println(trap(nums));
+        System.out.println(trap0(nums));
     }
 
     /**
@@ -53,11 +53,45 @@ public class _42_TrappingRainWater {
     }
 
     /**
-     * 韦恩图
-     * @param height
-     * @return
+     * 动态规划，使用两个中间数组额外保存左边的最高高度，右边的最高高度
+     * maxLeft[i] 表示第i列左边的最高高度
+     * maxRight[i] 表示第i列右边的最高高都
+     * <p>
+     * 如何求解maxLeft[i] ?
+     * 想要求得maxLeft[i]，如果能够知道第i列左边前一个列的左边最高高度maxLeft[i-1]，和左边前一个列的高度height[i-1]
+     * 那么 maxLeft[i] = max(maxLeft[i-1], height[i-1])
+     * 同理 maxRight[i] = max(maxRight[i+1], height[i+1])
      */
-    public static int trap0(int[] height){
+    private static int trap0(int[] height) {
+        if (height == null || height.length <= 1) {
+            return 0;
+        }
+        int n = height.length;
+        // 左边的最高高度缓存数组，默认初始化为0
+        int[] maxLeft = new int[n];
+        // 右边的最高高都缓存数组，默认初始化为0
+        int[] maxRight = new int[n];
+        // 填充maxLeft，从第二个元素开始遍历，因为第一列永远不能装下水
+        for (int i = 1; i < n; i++) {
+            maxLeft[i] = Math.max(maxLeft[i - 1], height[i - 1]);
+        }
+        // 填充maxRight，从倒数第二个元素开始遍历，因为最后一列永远不能装下水
+        for (int i = n - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1], height[i + 1]);
+        }
+        // 求解每一列能装的水，计算最终结果
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = Math.min(maxLeft[i], maxRight[i]) - height[i];
+            res += Math.max(temp, 0);
+        }
+        return res;
+    }
 
+    /**
+     * 韦恩图
+     */
+    public static int trap1(int[] height) {
+        return 0;
     }
 }
