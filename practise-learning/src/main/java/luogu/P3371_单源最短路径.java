97c8966197c1ceffc38e2a1b74b9cdff44a2961f
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2020-12-22 20:25:16 +0800

    最短路径问题 迪杰斯特拉算法(完成调试)

--- practise-learning/src/main/java/luogu/P3371_单源最短路径.java
+++ practise-learning/src/main/java/luogu/P3371_单源最短路径.java
@@ -1,7 +1,5 @@
 package luogu;
 
-import java.util.Scanner;
-
 /**
  * @author zhanglei
  * @version 1.0
@@ -33,106 +31,65 @@ import java.util.Scanner;
  **/
 public class P3371_单源最短路径 {
 
-    /**
-     * http://www.coding-daddy.com/algorithms/Dijkstra.html#_3-java%E5%AE%9E%E7%8E%B0
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         // 顶点个数
-        int n = 4;
-        // 有向边个数
-        int m = 6;
+        int n = 6;
         // 起点顶点编号
-        int s = 1;
+        int s = 0;
         // 使用邻接矩阵存储图
-        int[][] graph = null;
-        try (Scanner scanner = new Scanner(System.in)) {
-            //            n = scanner.nextInt();
-            //            m = scanner.nextInt();
-            //            s = scanner.nextInt();
-            graph = new int[n][n];
-            // 初始化所有图为正无穷，且当i=j的时候，节点路径为0
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (i == j) {
-                        graph[i][j] = 0;
-                    } else {
-                        graph[i][j] = Integer.MAX_VALUE;
-                    }
-                }
-            }
-            // 根据输入设置邻接矩阵
-            //            for (int i = 0; i < m; i++) {
-            //                graph[scanner.nextInt() - 1][scanner.nextInt() - 1] = scanner.nextInt();
-            //            }
-            graph = new int[][]{{0, 2, 5, 4}, {9999, 0, 2, 1}, {9999, 9999, 0, 3}, {9999, 9999, 9999, 0}};
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (graph != null) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(graph[i][j] + " ");
-                    if (j == 3) {
-                        System.out.print("\n");
-                    }
+        int max = 9999;
+        int[][] graph = new int[][]{{0, 1, 12, max, max, max}, {max, 0, 7, 3, max, max}, {max, max, 0, max, 5, max}, {max, max, 5, 0, 10, 13}, {max, max, max, max, 0, 4}, {max, max, max, max, max, 0}};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(graph[i][j] + "\t");
+                if (j == 5) {
+                    System.out.print("\n");
                 }
             }
         }
         int[] dis = dijkstra(graph, s);
-        for (int i = 0; i < dis.length; i++) {
-            System.out.print(dis[i] + " ");
+        for (int di : dis) {
+            System.out.print(di + " ");
         }
     }
 
-
-    /**
-     * 使用二维矩阵存储带权有向图
-     */
     private static int[] dijkstra(int[][] graph, int start) {
         int n = graph.length;
-        // dis集合存储起点s到每个顶点的最短路径
-        int[] dis = new int[n];
-        for (int i = 0; i < n; i++) {
-            dis[i] = 99999;
-        }
-        dis[start - 1] = 0;
-        // selected集合存储已经确定了的顶点，已经选择的为1，没有选择的为0
+        // dis集合存储起点s到每个顶点的最短路径，初始值为顶点s的权值，顶点s到自身的最短路径为0
+        int[] dis = graph[start];
+        dis[start] = 0;
+        // selected集合存储已经确定了的顶点，已经选择的为1，没有选择的为0，顶点s初始值为1
         int[] selected = new int[n];
-        selected[start - 1] = 1;
-        // 需要保存前驱节点
-        int[] pre = new int[n];
-        pre[start - 1] = 0;
-
+        selected[start] = 1;
+        // 需要一个指针保存每次找到的最近节点的位置
+        int pos = 0;
         // 进行n-1次遍历（因为起点已经选择了），完成dis集合的赋值
-        int pos = start - 1;
         for (int i = 0; i < n - 1; i++) {
             // 找出当前dis集合中可达的且最近的顶点值，并且放入selected集合，更新dis集合
             int tempMin = Integer.MAX_VALUE;
-            int k = pos;
             for (int j = 0; j < n; j++) {
-                // 当顶点没有被选择
+                // 只在还没有被选中的顶点集合中找
                 if (selected[j] != 1) {
-                    if (graph[k][j] < tempMin) {
-                        tempMin = graph[k][j];
-                        pos = j; // 此时pos指针已经指向当前遍历后所找到的最短路径的顶点位置
+                    // 只找距离顶点s最近的顶点
+                    if (dis[j] < tempMin) {
+                        tempMin = dis[j];
+                        pos = j;
+                    }
+                }
+            }
+            // 找到当前距离顶点s的最近顶点后，把这个顶点选出来，selecte集合置为1
+            selected[pos] = 1;
+            // 对dis集合进行松弛
+            for (int j = 0; j < n; j++) {
+                // 只在还没有被选中的顶点集合中进行松弛
+                if (selected[j] != 1) {
+                    if (dis[j] > graph[pos][j] + dis[pos]) {
+                        dis[j] = graph[pos][j] + dis[pos];
                     }
                 }
             }
 
-            // 把当前找到的最短路径更新dis集合，经过当前pos点，到达其他点的最短路径
-            for (int p = 0; p < n; p++) {
-                if (selected[p] != 1) {
-                    // 起点到pos点的距离 + pos点到p点的距离 < 起点到p点的距离
-                    if (graph[start - 1][pos] + graph[pos][p] <= graph[start - 1][p]) {
-                        dis[p] = graph[start - 1][pos] + graph[pos][p];
-                    }
-                }
-            } // 当前最小路径加入selected集合
-            selected[pos] = 1;
         }
         return dis;
     }
 }
-
