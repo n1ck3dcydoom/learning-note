364a4065806d7a3e3702926b41150e2027d3f907
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2021-01-07 20:01:17 +0800

    Leetcode 303. 区域和检索-数组不可变

---
+++ practise-learning/src/main/java/leetcode/dp/_303_RangeSumQuery_Immutable.java
@@ -0,0 +1,86 @@
+package leetcode.dp;
+
+/**
+ * @author zhanglei
+ * @version 1.0
+ * @description zl
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ * <p>
+ * 实现 NumArray 类：
+ * <p>
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ * <p>
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/1/7 19:29
+ **/
+public class _303_RangeSumQuery_Immutable {
+    public static void main(String[] args) {
+        NumArray numArray = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
+        System.out.println(numArray.sumRange(0, 2));
+        System.out.println(numArray.sumRange(2, 5));
+        System.out.println(numArray.sumRange(0, 5));
+    }
+}
+
+class NumArray {
+
+    private int[] dp;
+
+    public NumArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int n = nums.length;
+        int[] nums1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums1[i] = nums[i];
+        }
+        // 第一步、定义dp数组
+        // dp[i]表示nums从0到i的和
+
+        // 第二步、选择，递推表达式
+        // dp[i] = dp[i-1] + nums[i]
+
+        // 第三步、初始化
+        // dp[0] = nums[0];
+
+        this.dp = new int[n];
+        this.dp[0] = nums1[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0) {
+            return dp[j];
+        }
+        return dp[j] - dp[i - 1];
+    }
+}
