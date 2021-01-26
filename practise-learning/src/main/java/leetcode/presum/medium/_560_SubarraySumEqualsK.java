5bc760e69d38577444449b63aa7f4e6991c6ff66
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2021-01-26 22:39:36 +0800

    Leetcode 560. 和为K的子数组（字典存储前缀和次数，优化空间和时间）

--- practise-learning/src/main/java/leetcode/presum/medium/_560_SubarraySumEqualsK.java
+++ practise-learning/src/main/java/leetcode/presum/medium/_560_SubarraySumEqualsK.java
@@ -1,5 +1,7 @@
 package leetcode.presum.medium;
 
+import java.util.HashMap;
+
 /**
  * @author zhanglei
  * @version 1.0
@@ -49,6 +51,74 @@ public class _560_SubarraySumEqualsK {
                     count++;
                 }
             }
+        }
+        return count;
+    }
+
+    public int subarraySum1(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] preSum = new int[n + 1];
+        preSum[0] = 0;
+        // 求nums的前缀和数组
+        for (int i = 0; i < n; i++) {
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+        // 使用字典优化前缀和出现的次数存储
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // K = 3
+        // index 0 1 2  3  4  5
+        // nums  1 2 6  4 -1  5
+        // pre 0 1 3 9 13 12 17
+        // 当遍历到i=1的时候，pre=3，此时需要找map中有没有前缀和=0的
+        // 即pre = 3 - 3 = 0 的key，次数为1
+        // 所以需要开始初始化map放入(0,1)
+        // 表示 当前前缀和=K的时候，需要找map中有没有pre-K的key
+        map.put(0, 1);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(preSum[i + 1] - k)) {
+                count += map.get(preSum[i + 1] - k);
+            }
+            // 如果map里面含有pre-K的前缀和，则让他次数+1
+            // 如果没有，则放入次数1
+            map.put(preSum[i + 1], map.getOrDefault(preSum[i + 1], 0) + 1);
+        }
+        return count;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        // 使用字典优化前缀和出现的次数存储
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // K = 3
+        // index 0 1 2  3  4  5
+        // nums  1 2 6  4 -1  5
+        // pre 0 1 3 9 13 12 17
+        // 当遍历到i=1的时候，pre=3，此时需要找map中有没有前缀和=0的
+        // 即pre = 3 - 3 = 0 的key，次数为1
+        // 所以需要开始初始化map放入(0,1)
+        // 表示 当前前缀和=K的时候，需要找map中有没有pre-K的key
+
+        // 连前缀和数组都不用保存了，反正每次只关心map里面有没有前缀和-K的key存在
+        map.put(0, 1);
+        int count = 0;
+        int n = nums.length;
+        // 保存当前的前缀和
+        int preSum = 0;
+        for (int i = 0; i < n; i++) {
+            // 求当前的前缀和
+            preSum += nums[i];
+            if (map.containsKey(preSum - k)) {
+                count += map.get(preSum - k);
+            }
+            // 如果map里面含有pre-K的前缀和，则让他次数+1
+            // 如果没有，则放入次数1
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
         }
         return count;
     }
