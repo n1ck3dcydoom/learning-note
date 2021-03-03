797ff861e2ecbb90a33927f72c3c457cec5054d9
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2021-03-03 17:24:34 +0800

    Leetcode 64. 最小路径和 (二维朴素dp)

--- practise-learning/src/main/java/leetcode/dp/hard/_312_BurstBalloons.java
+++ practise-learning/src/main/java/leetcode/dp/hard/_312_BurstBalloons.java
@@ -35,13 +35,7 @@ package leetcode.dp.hard;
  * @date 2021/3/2 21:21
  **/
 public class _312_BurstBalloons {
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{1, 5};
-        System.out.println(maxCoins(nums));
-    }
-
-    public static int maxCoins(int[] nums) {
+    public int maxCoins(int[] nums) {
         // 对于某个区间i，j
         // 戳破其中第k个气球，可以得到的金币是nums[k-1] * nums[k] * nums[k+1]
         // 如果k-1或者k+1超过了边界，即k是区间的左端点，或者k是区间的右端点，那么特殊处理为金币等于1
@@ -53,6 +47,7 @@ public class _312_BurstBalloons {
         // 此时能够获得的最大金币dp[i][j] = dp[i][k] + nums[i] * nums[k] * nums[j] + dp[k][j]
         // 如果能够遍历i到j，计算出每个不同位置的k，那么求出最大值，就是区间(i,j)的最优解
 
+        int n = nums.length;
         // dp问题如何定义递推表达式，比较好的分析方式就是自底向上
         // 自底向上：对于区间(i,j)，思考 “最后” 一个被戳爆的气球，这里的 “最后” 一个，其实就是 “底”
 
@@ -61,7 +56,6 @@ public class _312_BurstBalloons {
         // 即  3,1,5,8 ->  1,3,1,5,8,1
         // 两头多出来的1，就是边界情况下的气球，但是我们又不能把他们戳爆
         // 所以dp数组需要多2个额外空间存储边界情况
-        int n = nums.length;
         int[][] dp = new int[n + 2][n + 2];
         // 对于给定的气球数组nums，也需要多开辟2个额外空间存储不存在的边界气球
         int[] newNums = new int[n + 2];
@@ -72,86 +66,14 @@ public class _312_BurstBalloons {
             newNums[i] = nums[i - 1];
         }
 
-        // 递归求解dp
-        return getMaxCoins(newNums, 0, n + 1, dp);
 
+        // 遍历区间长度
+        // 由于开区间，所以至少需要从3个气球开始遍历
+        for (int len = 3; len <= n + 2; len++) {
+
+        }
+
+
+        return -1;
     }
-
-    /**
-     * 如果用递归的话？会不会好想一点
-     * https://leetcode-cn.com/problems/burst-balloons/solution/java-dong-tai-gui-hua-xiao-bai-si-kao-guo-cheng-fe/
-     *
-     * @param nums  添加两端后的新金币数组
-     * @param start 当前需要递归的起点
-     * @param end   当前需要递归的终点
-     * @param dp    每次递归需要把dp数组带入
-     * @return 当前start到end的最大金币
-     */
-    private static int getMaxCoins(int[] nums, int start, int end, int[][] dp) {
-
-        // 第二步、思考结束递归的条件
-        // 什么情况下将没有气球能够被戳破了，当start = end - 1的时候，表示start和end之间再也没有气球了
-        // 这样自然就没有k值了，所以这个时候需要结束递归
-        // 既然没有k气球能够被戳破，自然也没有金币可以拿到，所以直接返回0
-        if (start == end - 1) {
-            return 0;
-        }
-
-        // 第一步、先思考递归的步骤
-        // 对于start和end，需要遍历中间每个k所分成的两个区间，求得k两端区间的最大硬币数后，加上戳破k的硬币数
-        // 对于每一个k值，求所有k值的最大值，即是dp[start][end]的最终结果
-        // 所以需要一个循环，从start开始，到end结束
-        // 需要一个变量保存每次遍历k得到的结果，计算最大值
-        int max = 0;
-
-        // nums:       3 1 5 8
-        // newNums:  1 3 1 5 8 1
-        // newIndex: 0 1 2 3 4 5
-
-        // oldLength = n = 4
-        // newLength = n + 2 = 6
-        // 第一次递归的start，自然是newNums的第一个，即下标0
-        // 第一次递归的end，自然是newNums的最后一个，即下标n+2-1= n+1
-        // 由于下标0和下标n+1的两个气球不能戳破（金币1），所以实际k值需要从start+1开始遍历，直到n+1
-        for (int k = start + 1; k < end; k++) {
-            // 比较每次遍历k得到的结果，保存最大值
-            // 这里的nums实际上是拓展后的newNums，所以下标不用额外处理
-            // nums[k] * nums[start] * nums[end]表示戳破最后一个气球k所能得到的金币数量
-            max = Math.max(max,
-                    nums[k] * nums[start] * nums[end] + getMaxCoins(nums, start, k, dp) + getMaxCoins(nums, k, end,
-                            dp));
-            // getMaxCoins(nums, start, k, dp) 表示求k左边的气球全部被戳爆后能得到的最大金币
-            // 由于递归遍历中k从start+1一直遍历到end-1为止，所以不用传入end = k-1，因为递归会从k=end - 1结束，即k永远不可能等于k-1了 (这样会导致k-1这个气球没有戳爆，但是k-1是一个合法的气球)
-            // getMaxCoins(nums, k, end, dp) 表示求k右边的气球全部被戳爆后能得到的最大金币
-            // 不用传入start = k + 1，因为递归会从start+1开始，即从k+2开始(这样会导致k+1这个气球没有被戳爆，但是k+1是一个合法的气球)
-        }
-        // 递归遍历完当前start到end的区间中所有合法气球被戳爆后，记录下dp[start][end]的值
-        dp[start][end] = max;
-        // 返回当前递归的结果
-        return max;
-    }
-
-    private static int getMaxCoins1(int[] nums, int start, int end, int[][] dp) {
-
-        if (start == end - 1) {
-            return 0;
-        }
-        // 直接递归会超时，考虑结束递归的条件
-
-        // 由于每次递归遍历左右区间时，当从start1到end1遍历到很小的区间start0到end0的时候
-        // 这里的start0到end0很有可能被之前更大的区间start2到end1遍历的时候计算过了
-        // 所以这里发现dp[start0][end0]的值已经不等于0的时候，可以提前结束递归
-        if (dp[start][end] != 0) {
-            return dp[start][end];
-        }
-        int max = 0;
-        for (int k = start + 1; k < end; k++) {
-            max = Math.max(max,
-                    nums[k] * nums[start] * nums[end] + getMaxCoins1(nums, start, k, dp) + getMaxCoins1(nums, k, end,
-                            dp));
-        }
-        dp[start][end] = max;
-        return max;
-    }
-
 }
--- practise-learning/src/main/java/leetcode/dp/medium/_467_UniqueSubstringsInWraparoundString.java
+++ practise-learning/src/main/java/leetcode/dp/medium/_467_UniqueSubstringsInWraparoundString.java
@@ -1,166 +1,166 @@
-package leetcode.dp.medium;
-
-/**
- * @author zhanglei
- * @version 1.0
- * @description leetcode
- * 把字符串 s 看作是“abcdefghijklmnopqrstuvwxyz”的无限环绕字符串，所以 s 看起来是这样的："...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd....". 
- * <p>
- * 现在我们有了另一个字符串 p 。你需要的是找出 s 中有多少个唯一的 p 的非空子串，尤其是当你的输入是字符串 p ，你需要输出字符串 s 中 p 的不同的非空子串的数目。 
- * <p>
- * 注意: p 仅由小写的英文字母组成，p 的大小可能超过 10000。
- * <p>
- *  
- * <p>
- * 示例 1:
- * <p>
- * 输入: "a"
- * 输出: 1
- * 解释: 字符串 S 中只有一个"a"子字符。
- *  
- * <p>
- * 示例 2:
- * <p>
- * 输入: "cac"
- * 输出: 2
- * 解释: 字符串 S 中的字符串“cac”只有两个子串“a”、“c”。.
- *  
- * <p>
- * 示例 3:
- * <p>
- * 输入: "zab"
- * 输出: 6
- * 解释: 在字符串 S 中有六个子串“z”、“a”、“b”、“za”、“ab”、“zab”。.
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/unique-substrings-in-wraparound-string
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- * @date 2021/1/14 23:15
- **/
-public class _467_UniqueSubstringsInWraparoundString {
-
-    public static void main(String[] args) {
-        System.out.println(findSubstringInWraproundString("cac"));
-    }
-
-    /**
-     * 题意：
-     * p的子串同时也是s的子串
-     * p的子串必须是连续的 注意：z和a是连续的
-     * <p>
-     * 即求p中每个字母结尾的所有连续子串的个数和
-     */
-    public static int findSubstringInWraproundString(String p) {
-
-        if (p == null || p.length() == 0) {
-            return 0;
-        }
-
-        // 第一步、定义dp[i] 表示当前选择当前字母后，和前面的连续子串和的“增量”
-
-        // 有点前缀和的意思
-
-        // 对于连续字符串 abcdef 来说
-        // 以a结尾的连续子串有 'a' 1个                                        a -> 1
-        // 以b结尾的连续子串有 'b' 'ab' 2个                                  ab -> 3
-        // 以c结尾的连续子串有 'c' 'bc' 'abc' 3个                           abc -> 6
-        // 以d结尾的连续子串有 'd' 'cd' 'bcd' 'abcd' 4个                   abcd -> 10
-        // 以e结尾的连续子串有 'e' 'de' 'cde' 'bcde' 'abcde' 5个          abcde -> 15
-        // 以f结尾的连续子串有 'f' 'df' 'edf' 'cdef' 'bcdef' 'abcdef'6个 abcdef -> 21
-        // 那么对于连续字符串 abcdef 来说，他的所有不重复的连续字符子串就有
-        // 以a结尾，以b结尾... 以f结尾的连续子串之和 1+2+3+4+5+6 = 21个
-
-        // 0 <= i <= 25
-        // 第一步、定义dp[i] 表示以字母i(26字母表中顺序)结尾的连续子串和
-        int[] dp = new int[26];
-
-        // 第二步、初始值
-        // 只有字母a的时候，连续子串和为1
-        // p开头的第一个字符构成初始值
-        dp[p.charAt(0) - 'a'] = 1;
-
-
-        // 第三步、选择，递推表达式
-        // 如果dp[i]与前一个dp[i-1]连续，那么dp[i] = dp[i-1] + (pos[i] + 1)
-        // 例如，abcde，以字母e结尾的连续子串和dp[4] = dp[3] + (pos[e] + 1)
-        //                                  dp[4] = dp[3] + 4 + 1 = 15
-
-        // 如果dp[i]与前一个dp[i-1]不连续，那么dp[i] = dp[i-1] + 1
-        // 前面连续的i-1个子串和 + 第i个不连续的字符本身
-        // abcdx 以x结尾的连续子串和 dp[4] = dp[3] + 1
-        // dp[3] = 10 但是x和d不连续，所以只能是 a,b,ab,c,bc,abc,d,cd,bcd,abcd 这10个
-        // 加上x本身 即dp[4] = dp[3] + 1 = 11
-        // 所以题目的答案就是dp[1] + dp[2] + ... + dp[26]的和
-        // 即p串中出现的所有字符结尾的连续子串之和sum
-
-        // 这里是有问题的，如果字符串 zabcdabc
-        // 其中zabc abc  后面的abc已经被zabc包含了，在计算c结尾的子串中，后面的abc是重复的
-        // 同理 a, b结尾的子串，也是重复的
-
-        // 考虑如果已经计算了zabc 以c结尾的连续子串长度为c1
-        // 第二次计算abc的时候，又发现了以c结尾的连续子串长度为c2
-        // 如果c1 > c2 表示之前计算的以c结尾的连续子串长度比现在这个c结尾的药厂
-        // 说明之前的以c结尾的连续子串长度 要比 现在这个以c结尾的连续子串长度要长
-        // c1 应该是包含了 c2 ， c2是c1的重复子串，这个时候什么也不做
-
-        // 如果c1 < c2 表示之前以c结尾的连续子串长度比现在c结尾这个要短
-        // sum = sum + (c2 - c1)  其中c2-c1表示更长的以c结尾比前面短的c结尾的，多出来的，还没有被计算的连续子串
-        // 例如 abczabc -> abc      zabc
-        //                 c1 = 6   c2 = 10
-        // 其中的 z,za,zab,zabc 就是前面c1没有计算的
-        // 更新了sum之后，还要把c2和c1的较大值重新赋值回去，以便于和c3、c4等等作比较
-
-        // p的第一个字符已经当作初始值，从i=1，即第二个字符开始遍历p串
-
-
-        // HashMap<Character, Integer> index = new HashMap<>(26);
-        int[] index = new int[26];
-        //index.put(p.charAt(0), dp[p.charAt(0) - 'a']);
-        index[p.charAt(0) - 'a'] = dp[p.charAt(0) - 'a']; // 初始化p串的第一个字符的索引表值
-
-        int sum = dp[p.charAt(0) - 'a'];
-        for (int i = 1; i < p.length(); i++) {
-            // 如果i和i+1连续
-            if (continuity(p.charAt(i - 1), p.charAt(i))) {
-                // abcde
-                // 其中a被初始化dp[0] = 1
-                // b和a连续 dp[1] = dp[1-0] + pos[1] + 1
-                // b在26个字母中是第二个，数组pos的下标是1
-                //         dp[1] = dp[0] + 1 + 1 = 3
-                dp[p.charAt(i) - 'a'] = dp[p.charAt(i - 1) - 'a'] + i + 1;
-                int pre = index.get(p.charAt(i)) == null ? 0 : index.get(p.charAt(i));
-                // c2 > c1
-                if (pre == 0) {
-                    index.put(p.charAt(i), dp[p.charAt(i) - 'a']);
-                    sum += dp[p.charAt(i) - 'a'] - dp[p.charAt(i - 1) - 'a'];
-                } else {
-                    if (dp[p.charAt(i) - 'a'] > pre) {
-                        index.put(p.charAt(i), dp[p.charAt(i) - 'a']);
-                        sum += dp[p.charAt(i) - 'a'] - pre;
-                    }
-                }
-            } else {
-                dp[p.charAt(i) - 'a'] = dp[p.charAt(i - 1) - 'a'] + 1;
-                index.put(p.charAt(i), dp[p.charAt(i) - 'a']);
-                int pre = index.get(p.charAt(i)) == null ? 0 : index.get(p.charAt(i));
-                if (pre == 0) {
-                    sum += 1;
-                }
-            }
-        }
-
-        return sum;
-    }
-
-    /**
-     * 判断两个字符是否连续
-     * 注意：za 连续 az不连续
-     */
-    private static boolean continuity(char a, char b) {
-        if ('z' == a) {
-            return 'a' == b;
-        } else {
-            return b - a == 1;
-        }
-    }
-}
+//package leetcode.dp.medium;
+//
+///**
+// * @author zhanglei
+// * @version 1.0
+// * @description leetcode
+// * 把字符串 s 看作是“abcdefghijklmnopqrstuvwxyz”的无限环绕字符串，所以 s 看起来是这样的："...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd....". 
+// * <p>
+// * 现在我们有了另一个字符串 p 。你需要的是找出 s 中有多少个唯一的 p 的非空子串，尤其是当你的输入是字符串 p ，你需要输出字符串 s 中 p 的不同的非空子串的数目。 
+// * <p>
+// * 注意: p 仅由小写的英文字母组成，p 的大小可能超过 10000。
+// * <p>
+// *  
+// * <p>
+// * 示例 1:
+// * <p>
+// * 输入: "a"
+// * 输出: 1
+// * 解释: 字符串 S 中只有一个"a"子字符。
+// *  
+// * <p>
+// * 示例 2:
+// * <p>
+// * 输入: "cac"
+// * 输出: 2
+// * 解释: 字符串 S 中的字符串“cac”只有两个子串“a”、“c”。.
+// *  
+// * <p>
+// * 示例 3:
+// * <p>
+// * 输入: "zab"
+// * 输出: 6
+// * 解释: 在字符串 S 中有六个子串“z”、“a”、“b”、“za”、“ab”、“zab”。.
+// * <p>
+// * 来源：力扣（LeetCode）
+// * 链接：https://leetcode-cn.com/problems/unique-substrings-in-wraparound-string
+// * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+// * @date 2021/1/14 23:15
+// **/
+//public class _467_UniqueSubstringsInWraparoundString {
+//
+//    public static void main(String[] args) {
+//        System.out.println(findSubstringInWraproundString("cac"));
+//    }
+//
+//    /**
+//     * 题意：
+//     * p的子串同时也是s的子串
+//     * p的子串必须是连续的 注意：z和a是连续的
+//     * <p>
+//     * 即求p中每个字母结尾的所有连续子串的个数和
+//     */
+//    public static int findSubstringInWraproundString(String p) {
+//
+//        if (p == null || p.length() == 0) {
+//            return 0;
+//        }
+//
+//        // 第一步、定义dp[i] 表示当前选择当前字母后，和前面的连续子串和的“增量”
+//
+//        // 有点前缀和的意思
+//
+//        // 对于连续字符串 abcdef 来说
+//        // 以a结尾的连续子串有 'a' 1个                                        a -> 1
+//        // 以b结尾的连续子串有 'b' 'ab' 2个                                  ab -> 3
+//        // 以c结尾的连续子串有 'c' 'bc' 'abc' 3个                           abc -> 6
+//        // 以d结尾的连续子串有 'd' 'cd' 'bcd' 'abcd' 4个                   abcd -> 10
+//        // 以e结尾的连续子串有 'e' 'de' 'cde' 'bcde' 'abcde' 5个          abcde -> 15
+//        // 以f结尾的连续子串有 'f' 'df' 'edf' 'cdef' 'bcdef' 'abcdef'6个 abcdef -> 21
+//        // 那么对于连续字符串 abcdef 来说，他的所有不重复的连续字符子串就有
+//        // 以a结尾，以b结尾... 以f结尾的连续子串之和 1+2+3+4+5+6 = 21个
+//
+//        // 0 <= i <= 25
+//        // 第一步、定义dp[i] 表示以字母i(26字母表中顺序)结尾的连续子串和
+//        int[] dp = new int[26];
+//
+//        // 第二步、初始值
+//        // 只有字母a的时候，连续子串和为1
+//        // p开头的第一个字符构成初始值
+//        dp[p.charAt(0) - 'a'] = 1;
+//
+//
+//        // 第三步、选择，递推表达式
+//        // 如果dp[i]与前一个dp[i-1]连续，那么dp[i] = dp[i-1] + (pos[i] + 1)
+//        // 例如，abcde，以字母e结尾的连续子串和dp[4] = dp[3] + (pos[e] + 1)
+//        //                                  dp[4] = dp[3] + 4 + 1 = 15
+//
+//        // 如果dp[i]与前一个dp[i-1]不连续，那么dp[i] = dp[i-1] + 1
+//        // 前面连续的i-1个子串和 + 第i个不连续的字符本身
+//        // abcdx 以x结尾的连续子串和 dp[4] = dp[3] + 1
+//        // dp[3] = 10 但是x和d不连续，所以只能是 a,b,ab,c,bc,abc,d,cd,bcd,abcd 这10个
+//        // 加上x本身 即dp[4] = dp[3] + 1 = 11
+//        // 所以题目的答案就是dp[1] + dp[2] + ... + dp[26]的和
+//        // 即p串中出现的所有字符结尾的连续子串之和sum
+//
+//        // 这里是有问题的，如果字符串 zabcdabc
+//        // 其中zabc abc  后面的abc已经被zabc包含了，在计算c结尾的子串中，后面的abc是重复的
+//        // 同理 a, b结尾的子串，也是重复的
+//
+//        // 考虑如果已经计算了zabc 以c结尾的连续子串长度为c1
+//        // 第二次计算abc的时候，又发现了以c结尾的连续子串长度为c2
+//        // 如果c1 > c2 表示之前计算的以c结尾的连续子串长度比现在这个c结尾的药厂
+//        // 说明之前的以c结尾的连续子串长度 要比 现在这个以c结尾的连续子串长度要长
+//        // c1 应该是包含了 c2 ， c2是c1的重复子串，这个时候什么也不做
+//
+//        // 如果c1 < c2 表示之前以c结尾的连续子串长度比现在c结尾这个要短
+//        // sum = sum + (c2 - c1)  其中c2-c1表示更长的以c结尾比前面短的c结尾的，多出来的，还没有被计算的连续子串
+//        // 例如 abczabc -> abc      zabc
+//        //                 c1 = 6   c2 = 10
+//        // 其中的 z,za,zab,zabc 就是前面c1没有计算的
+//        // 更新了sum之后，还要把c2和c1的较大值重新赋值回去，以便于和c3、c4等等作比较
+//
+//        // p的第一个字符已经当作初始值，从i=1，即第二个字符开始遍历p串
+//
+//
+//        // HashMap<Character, Integer> index = new HashMap<>(26);
+//        int[] index = new int[26];
+//        //index.put(p.charAt(0), dp[p.charAt(0) - 'a']);
+//        index[p.charAt(0) - 'a'] = dp[p.charAt(0) - 'a']; // 初始化p串的第一个字符的索引表值
+//
+//        int sum = dp[p.charAt(0) - 'a'];
+//        for (int i = 1; i < p.length(); i++) {
+//            // 如果i和i+1连续
+//            if (continuity(p.charAt(i - 1), p.charAt(i))) {
+//                // abcde
+//                // 其中a被初始化dp[0] = 1
+//                // b和a连续 dp[1] = dp[1-0] + pos[1] + 1
+//                // b在26个字母中是第二个，数组pos的下标是1
+//                //         dp[1] = dp[0] + 1 + 1 = 3
+//                dp[p.charAt(i) - 'a'] = dp[p.charAt(i - 1) - 'a'] + i + 1;
+//                int pre = index.get(p.charAt(i)) == null ? 0 : index.get(p.charAt(i));
+//                // c2 > c1
+//                if (pre == 0) {
+//                    index.put(p.charAt(i), dp[p.charAt(i) - 'a']);
+//                    sum += dp[p.charAt(i) - 'a'] - dp[p.charAt(i - 1) - 'a'];
+//                } else {
+//                    if (dp[p.charAt(i) - 'a'] > pre) {
+//                        index.put(p.charAt(i), dp[p.charAt(i) - 'a']);
+//                        sum += dp[p.charAt(i) - 'a'] - pre;
+//                    }
+//                }
+//            } else {
+//                dp[p.charAt(i) - 'a'] = dp[p.charAt(i - 1) - 'a'] + 1;
+//                index.put(p.charAt(i), dp[p.charAt(i) - 'a']);
+//                int pre = index.get(p.charAt(i)) == null ? 0 : index.get(p.charAt(i));
+//                if (pre == 0) {
+//                    sum += 1;
+//                }
+//            }
+//        }
+//
+//        return sum;
+//    }
+//
+//    /**
+//     * 判断两个字符是否连续
+//     * 注意：za 连续 az不连续
+//     */
+//    private static boolean continuity(char a, char b) {
+//        if ('z' == a) {
+//            return 'a' == b;
+//        } else {
+//            return b - a == 1;
+//        }
+//    }
+//}
