3edc762b079b7025be5deded72372a30969422b4
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2021-04-21 14:30:50 +0800

    Java Future

---
+++ java-learning/src/main/java/future/Main.java
@@ -0,0 +1,16 @@
+//package future;
+//
+//import java.util.concurrent.Future;
+//import java.util.concurrent.FutureTask;
+//
+///**
+// * @author zhanglei
+// * @version 1.0
+// * @description TODO
+// * @date 2021/2/25 13:32
+// **/
+//public class Main {
+//    public static void main(String[] args) {
+//        Future future = new FutureTask();
+//    }
+//}
