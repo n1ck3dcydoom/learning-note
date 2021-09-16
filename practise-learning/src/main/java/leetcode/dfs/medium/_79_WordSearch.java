f8b0645423c2a9db1a36a9b0841823b304825ede
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2021-09-16 22:12:29 +0800

    Leetcode 79. 单词搜索 (dfs+回溯)

--- practise-learning/src/main/java/leetcode/dfs/medium/_79_WordSearch.java
+++ practise-learning/src/main/java/leetcode/dfs/medium/_79_WordSearch.java
@@ -1,77 +1,63 @@
 package leetcode.dfs.medium;
 
-/**
- * @version 1.0
- * @description leetcode
- * <p>
- * 78. 单词搜索
- * @date 2021/5/21 14:58
- **/
 public class _79_WordSearch {
 
-    //    public static void main(String[] args) {
-    //        char[][] board = new char[][]{{'a', 'b', 'c', 'e'}, {'s', 'f', 'c', 's'}, {'a', 'd', 'e', 'e'}};
-    //        String word = "abcb";
-    //        System.out.println(exist(board, word));
-    //    }
+    public static void main(String[] args) {
+        char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        String word1 = "ABCCED";
+        String word2 = "SEE";
+        String word3 = "ABCB";
+        System.out.println(exist(board, word1));
+        // System.out.println(exist(board, word2));
+        // System.out.println(exist(board, word3));
+    }
 
-    private boolean res = false;
-
-    public boolean exist(char[][] board, String word) {
-        // 有点类似岛屿问题
-        // 直接dfs搜索
-
-        // 行数
-        int m = board.length;
-        // 列数
-        int n = board[0].length;
-        // 辅助数组，保存当前格子是否已经被路径访问了
-        boolean[][] visited = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (res) {
-                    break;
-                }
-                // 如果当前格子不是单词的开头，直接跳过
+    public static boolean exist(char[][] board, String word) {
+        // dfs深搜
+        // 访问数组
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        // 只从字母开头字符开始寻找
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
-                    dfs(board, visited, i, j, word, 0);
+                    if (dfs(board, visited, word, 0, i, j)) {
+                        return true;
+                    }
                 }
             }
         }
-        return res;
+        return false;
     }
 
-    private void dfs(char[][] board, boolean[][] visited, int i, int j, String word, int index) {
-        // 找到所有退出dfs的情况
+    private static boolean dfs(char[][] board, boolean[][] visited, String word, int index, int x, int y) {
+        // 如果超出边界、已经访问过、不等于需要寻找的字符
+        if (!inSide(board, visited, x, y)) {
+            return false;
+        }
+        if (index == word.length() - 1) {
+            return board[x][y] == word.charAt(index);
+        }
+        // 没有匹配上
+        if (board[x][y] != word.charAt(index)) {
+            return false;
+        }
+        // 标记当前格子已经被访问过
+        visited[x][y] = true;
 
-        // 超出边界，或者当前格子已经被访问过
-        if (!arrivedSide(board, i, j) || visited[i][j]) {
-            return;
+        int[][] search = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < 4; i++) {
+            int[] to = search[i];
+            // 只要有一个方向找到下一个字符了，就返回true
+            if (dfs(board, visited, word, index + 1, x + to[0], y + to[1])) {
+                return true;
+            }
         }
-        // index还没有等于单词的长度，但是当前格子已经不是word的下一个字符了
-        if (index < word.length() && board[i][j] != word.charAt(index)) {
-            return;
-        }
-        // 找到单词了！
-        // index等于单词的长度，且当前格子的字符等于单词的最后一个字符
-        if (index == word.length() - 1 && board[i][j] == word.charAt(index)) {
-            res = true;
-        }
-        // 还没有找到结果，且当前格子等于当前单词的索引，才继续dfs，否则就返回
-        if (!res && board[i][j] == word.charAt(index)) {
-            // 标记当前格子已经被访问了
-            visited[i][j] = true;
-            // 往四个方向上进行dfs，有一个方向上找到了就返回true
-            dfs(board, visited, i + 1, j, word, index + 1);
-            dfs(board, visited, i - 1, j, word, index + 1);
-            dfs(board, visited, i, j + 1, word, index + 1);
-            dfs(board, visited, i, j - 1, word, index + 1);
-            // 回溯需要重置访问状态
-            visited[i][j] = false;
-        }
+        // 回溯，撤销选择
+        visited[x][y] = false;
+        return false;
     }
 
-    private boolean arrivedSide(char[][] board, int i, int j) {
-        return (0 <= i && i < board.length) && (0 <= j && j < board[0].length);
+    private static boolean inSide(char[][] board, boolean[][] visited, int x, int y) {
+        return (0 <= x && x < board.length) && (0 <= y && y < board[0].length) && !visited[x][y];
     }
 }
