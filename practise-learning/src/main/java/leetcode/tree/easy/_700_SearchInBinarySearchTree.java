dc7a67b13111542da34ec3f0d7907ddfd999d5fd
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2021-11-26 17:48:29 +0800

    Leetcode 700. 二叉搜索树中的搜索 (朴素dfs)

---
+++ practise-learning/src/main/java/leetcode/tree/easy/_700_SearchInBinarySearchTree.java
@@ -0,0 +1,30 @@
+package leetcode.tree.easy;
+
+import leetcode.data.TreeNode;
+
+public class _700_SearchInBinarySearchTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        searchBST(root, 2);
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
