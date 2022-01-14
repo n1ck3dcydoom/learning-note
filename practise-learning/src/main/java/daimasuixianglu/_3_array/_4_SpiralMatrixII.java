4d906a7a406b60f75e61a772f182485f24cc53fa
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2022-01-14 18:04:15 +0800

    代码随想录 1数组 螺旋数组II (边界条件控制)

---
+++ practise-learning/src/main/java/daimasuixianglu/_3_array/_4_SpiralMatrixII.java
@@ -0,0 +1,51 @@
+package daimasuixianglu._3_array;
+
+public class _4_SpiralMatrixII {
+
+    public static void main(String[] args) {
+        int[][] res = generateMatrix(7);
+        for (int[] nums : res) {
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public static int[][] generateMatrix(int n) {
+        // 矩阵边长等于n
+        int[][] res = new int[n][n];
+        // 定义四个边界top bottom left right
+        int top = 0, left = 0;
+        int bottom = n - 1, right = n - 1;
+        // 填充数从[1,n]
+        int num = 1;
+        while (num <= n * n) {
+            // 上边界填充顺序从左往右
+            int l = left;
+            while (l <= right) {
+                res[top][l++] = num++;
+            }
+            top++;
+            // 右边界填充顺序从上往下
+            int t = top;
+            while (t <= bottom) {
+                res[t++][right] = num++;
+            }
+            right--;
+            // 下边界填充顺序从右往左
+            int r = right;
+            while (r >= left) {
+                res[bottom][r--] = num++;
+            }
+            bottom--;
+            // 左边界填充顺序从下往上
+            int b = bottom;
+            while (b >= top) {
+                res[b--][left] = num++;
+            }
+            left++;
+        }
+        return res;
+    }
+}
